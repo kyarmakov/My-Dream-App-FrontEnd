c74e26b4c71a4167b8b0a6982a68bee633c02e6a
Kerim Yarmakov <kyarmakov@gmail.com>
2022-12-06 17:01:09 +0300

    added home.module and routing for home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-const routes: Routes = [];
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./pages/home/home.component";
+
+const routes: Routes = [
+  {
+    path: '', component: AppComponent, children: [
+      {path: '', redirectTo: '/', pathMatch: 'full'},
+      {path: '', component: HomeComponent}
+    ],
+  },
+  {path: '**', redirectTo:'/', pathMatch: 'full'}
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
