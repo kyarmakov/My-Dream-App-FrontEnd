d08316c5e56a1123135e635434a70c029dbdf414
Kerim Yarmakov <kyarmakov@gmail.com>
2022-12-08 11:33:11 +0300

    created header component and added to home page instead of app component, otherwise it is rendered twice.

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,9 +3,13 @@ import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 import { HomeComponent } from './home.component';
+import { HeaderComponent } from 'src/app/components/header/header.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [
+    HomeComponent,
+    HeaderComponent
+  ],
   imports: [
     CommonModule,
     RouterModule.forChild([{ path: '', component: HomeComponent }]),
